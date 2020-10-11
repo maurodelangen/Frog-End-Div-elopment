c1faaab1e549c792fb78832c2e68cd53d1a4d271
maurodelangen <55748069+maurodelangen@users.noreply.github.com>
2020-10-11 22:18:41 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,14 @@
-// JavaScript Document
+var a = document.querySelector('.hamburger');
+var b = false;
+const c = document.querySelector('nav')
+a.addEventListener('click', function () {
+    if (!b) {
+        c.style.setProperty("display", "block");
+        console.log('text');
+        b = true;
+    } else {
+       b = false;
+     c.style.setProperty("display", "none");
+        console.log('poop');
+    }
+},false);
